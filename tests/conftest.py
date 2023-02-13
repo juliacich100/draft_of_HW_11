08c7170d4bdb542e644ce8a4f8e7bf13eeb9bbef
iuliiach <h46@yandex.ru>
2023-02-13 20:16:55 +0300

    added tests parametrized with indirect

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,15 @@ def mobile_browser_management():
     browser.config.window_width = 412
     yield
     browser.quit()
+
+
+@pytest.fixture(params=['1080*1920', '914*412'], scope='session')
+def browser_window_size(request):
+    if request.param == '1080*1920':
+        browser.config.window_height = 1080
+        browser.config.window_width = 1920
+    else:
+        browser.config.window_height = 914
+        browser.config.window_width = 412
+    yield
+    browser.quit()
--- tests/test_github.py
+++ tests/test_github.py
@@ -1,8 +1,8 @@
 import pytest
 from selene.support.shared import browser
-#  import pytest
 
-'''Tests without parametrization'''
+
+'''Tests without parametrization with different fixtures'''
 
 
 def test_desktop_sign_in(desktop_browser_management):
@@ -54,6 +54,22 @@ def test_only_desktop_sign_in(window_height, window_width):
 def test_only_mobile_sign_in(window_height, window_width):
     browser.config.window_height = window_height
     browser.config.window_width = window_width
+    browser.open('https://github.com/')
+    browser.element('.Button--link').click()
+    browser.element('[href="/login"]').click()
+
+
+'''Parametrization with indirect'''
+
+
+@pytest.mark.parametrize('browser_window_size', ['1080*1920'], indirect=True)
+def test_desktop_sign_in_with_indirect(browser_window_size):
+    browser.open('https://github.com/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+
+
+@pytest.mark.parametrize('browser_window_size', ['914*412'], indirect=True)
+def test_mobile_sign_in_with_indirect(browser_window_size):
     browser.open('https://github.com/')
     browser.element('.Button--link').click()
     browser.element('[href="/login"]').click()
